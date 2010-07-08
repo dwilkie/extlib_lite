f359100b93b87fd6c2b46fae8d6ff3054e4040fe
David Wilkie <dwilkie@gmail.com>
2010-07-08 12:15:33 +0700

    fixed loading of extensions

--- lib/extlib_lite/core_extensions.rb
+++ lib/extlib_lite/core_extensions.rb
@@ -1,4 +1,6 @@
 require 'addressable/uri'
-require 'core_extensions/hash'
-require 'core_extensions/string'
+# Load each available extension
+Dir["#{File.dirname(__FILE__)}/core_extensions/*.rb"].sort.each do |path|
+  require "extlib_lite/core_extensions/#{File.basename(path)}"
+end
 
